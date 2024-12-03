b254656cb7a64732f5d36e5d0f8194d54564bfa7
huikkoona <oona.huikko@gmail.com>
2024-12-03 15:27:06 +0200

    numeropeli ensimmäinen versio

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import random
+
+# Tietokone arpoo numeron väliltä 1-100
+oikea_numero = random.randint(1, 100)
+
 print("Tervetuloa Numeron Arvaus -peliin!")
+print("Tietokone on arvonnut numeron väliltä 1-100.")
+
+while True:
+    arvaus = int(input("Anna arvauksesi: "))
+
+    if arvaus < oikea_numero:
+        print("Liian pieni!")
+    elif arvaus > oikea_numero:
+        print("Liian suuri!")
+    else:
+        print("Oikein! Numero oli:", oikea_numero)
+        break
